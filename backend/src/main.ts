7c8ab3e6df17c2c5563f31336ca4191baa7f982c
reinaldoper <reinaldoper83@gmail.com>
2025-12-16 10:56:06 -0400

    fix: Adiciona rota para deploy do frontend.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,10 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://frontend:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://tecnologia-esportiva.netlify.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
